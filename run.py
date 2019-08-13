ba96f16345e1fb47ac6a7130719fbe8978295e31
Nic Dobbins <nicdobbins@D-10-146-21-99.dhcp4.washington.edu>
2019-08-13 07:54:42 -0700

    Updated union()

--- run.py
+++ run.py
@@ -72,7 +72,7 @@ def union():
     # Check first annotator
     for anno in anno1:
 
-        # Find matching Brat file
+        # Find matching Brat file in annotator2
         matches = [brat for brat in anno2 if brat.path == anno.path]
         cnt = len(matches)
 
@@ -91,20 +91,13 @@ def union():
     # Check second annotator
     for anno in anno2:
 
-        # Find matching Brat file
+        # Find matching Brat file in annotator 1
         matches = [brat for brat in anno1 if brat.path == anno.path]
-        cnt = len(matches)
 
-        if cnt == 0:
-            print(f'No matches found for file "{anno.path}". Saving single annotation file without union...')
+        # If none, only the second annotator has this file and
+        # it has not been processed, so save as gold.
+        if len(matches) == 0:
+            print(f'File "{anno.path}" was only present in the second annotator''s directory. Saving single annotation file without union...')
             save_gold_file(out_dir, anno)
-            continue
-
-        if cnt > 1:
-            print(f'{cnt} matches were unexpectedly found for "{anno.path}". Taking only the first...')
-
-        match = matches[0]
-        gold = anno.union(match)
-        save_gold_file(out_dir, gold)
     
-intersect()
+union()
