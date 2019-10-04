868aa4c8cb335957c374eafd7754fc9363edbba5
Nic Dobbins <nicdobbins@Nics-MacBook-Pro.local>
2019-10-04 06:54:23 -0700

    Refactoring

--- src/brat_file.py
+++ src/brat_file.py
@@ -17,7 +17,12 @@ class BratFile:
                     if (row.valid()):
                         self.rows.append(BratRow(line))
 
-    def get_match(self, comparator):
+    def get_row_match(self, comparator, strict):
+        if strict == True:
+            return get_strict_match(self, comparator)
+        return get_overlap_label_match(self, comparator)
+
+    def get_strict_match(self, comparator):
 
         # Look for a match by exact indices
         matches = [row for row in self.rows if 
@@ -29,6 +34,55 @@ class BratFile:
         if len(matches) > 0:
             return matches[0]
     
+        return None
+
+    def get_overlap_label_match(self, comparator):
+
+        for row in self.rows:
+
+            A = self
+            B = comparator
+
+            ''' 1. If there are two overlapping spans with the same label - 
+                include in the gold standard the span with the longest span 
+                (delete the shorter one) - create a log of it. 
+            '''
+            # Same label
+            if \
+                A.indices.index_type == B.indices.index_type and \
+                (
+                    # A begins inside B
+                    A.indices.start > B.indices.start and
+                    A.indices.start < B.indices.end
+                ) or \
+                (
+                    # B begins inside A
+                    B.indices.start > A.indices.start and
+                    B.indices.start < A.indices.end
+                ):
+                lenA = len(A.indices.end - A.indices.start) 
+                lenB = len(B.indices.end - B.indices.start)
+
+                if (lenA > lenB):
+                    return [ A ]
+                else:
+                    return [ B ]
+
+            # Same span
+            elif A.indices.start == B.indices.start and \
+                A.indices.end == B.indices.end:
+
+                ''' 2. if the same span has two different labels — 
+                    include both labels and create a log of it.
+                '''
+
+                # Different label
+                if A.indices.index_type != B.indices.index_type:
+                    return [ A, B ]
+
+                # Same label
+                return [ A ]
+
         return None
 
     def intersect(self, comparator):
@@ -56,7 +110,7 @@ class BratFile:
             gold.rows.append(new_row)
 
         for row in comparator.rows:
-            existing = gold.get_match(row)
+            existing = gold.get_row_match(row, False)
             if existing is None:
                 new_row = copy(row)
                 gold.rows.append(new_row)
